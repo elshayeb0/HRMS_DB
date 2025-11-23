671dacfae2104f6b8860eb514bea3c2bbcd78615
yassinzaki16005309 <amryassin2006@gmail.com>
2025-11-23 14:30:27 +0200

    Implemented all prodecures in HR

--- db/procedures/02_hr_admin.sql
+++ db/procedures/02_hr_admin.sql
@@ -10,4 +10,28 @@
 USE HRMS_DB;
 GO
 
---1
+-- 1:
+CREATE PROCEDURE CreateContract
+    @EmployeeID INT,
+    @Type VARCHAR(50),
+    @StartDate DATE,
+    @EndDate DATE, 
+AS
+BEGIN
+
+    SET NOCOUNT ON;
+
+    -- Insert new contract
+    INSERT INTO Contract (type, start_date, end_date, current_state)
+    VALUES (@Type, @StartDate, @EndDate, 'Active');
+
+    -- Link contract to employee
+    UPDATE Employee
+    SET contract_id = SCOPE_IDENTITY()
+    WHERE employee_id = @EmployeeID;
+
+    SELECT 'Contract created successfully for Employee ID: ' + CAST(@EmployeeID AS VARCHAR) + 
+       '. Contract ID: ' + CAST(SCOPE_IDENTITY() AS VARCHAR) AS Message;
+
+END;
+
